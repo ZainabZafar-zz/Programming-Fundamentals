305e1f06905686f5c8802878185abb541532d1ac
ZainabZafar-zz <zainabzafarbhatti@gmail.com>
2025-12-22 22:58:39 +0500

    Add files via upload

---
+++ 5.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int sum = 0, count;
+
+    for (count = 1; count <= 99; count += 2) {
+        sum += count;
+    }
+
+    cout << "Sum of odd integers between 1 and 99 is: " << sum << endl;
+    return 0;
+}
+
